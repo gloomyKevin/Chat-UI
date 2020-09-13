cc0dfdd61544f83f95286676185e7dddc991101e
gloomykevin <1293535916@qq.com>
2020-09-13 20:22:37 +0800

    configure environment

---
+++ .storybook/preview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react'
+import { themeProvider } from "../src/theme"
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
