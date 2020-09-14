ce1efdd1967a687445ac0cdc8a479c809742b476
gloomykevin <1293535916@qq.com>
2020-09-14 15:59:33 +0800

    add Icon

---
+++ src/components/Icon/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import StyledIcon from "./style";
+
+function Icon({
+    children,
+    ...rest
+}) {
+    return (
+        <StyledIcon {...rest}>
+            {children}
+        </StyledIcon>
+    );
+}
+
+Icon.propTypes = {
+    children: PropTypes.any
+};
+
+export default Icon;
